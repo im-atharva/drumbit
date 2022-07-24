9eed9de80322ad9ef668eb3cefec82e406e6c716
Atharva Meshram <106589084+im-atharva@users.noreply.github.com>
2022-07-24 19:13:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-//? document.querySelector(".w ").addEventListener("click", clicked);
-//!note we are not using a () after the function! coz we are not calling the function we are just passing the function.
 
-/*Same thing with other method (anonymous function)
-
-document.querySelector("button").addEventListener("click", function() {
-    alert("I got Clicked");
-});  
- */
-
-//* button press area!
 
 var noOfDrumbeats = document.querySelectorAll(".drum").length;
 for (var i = 0; i < noOfDrumbeats; i++) {
@@ -21,35 +11,35 @@ function clicked() {
 
     switch (buttonInnerHTML) {
         case "w":
-            var tom1 = new Audio('sounds/tom-1.mp3');
+            var tom1 = new Audio('tom-1.mp3');
             tom1.play();
 
         case "a":
-            var tom2 = new Audio('sounds/tom-2.mp3');
+            var tom2 = new Audio('tom-2.mp3');
             tom2.play();
             break;
         case "s":
-            var tom3 = new Audio('sounds/tom-3.mp3');
+            var tom3 = new Audio('tom-3.mp3');
             tom3.play();
             break;
 
         case "d":
-            var tom4 = new Audio('sounds/tom-4.mp3');
+            var tom4 = new Audio('tom-4.mp3');
             tom4.play();
             break;
 
         case "j":
-            var crash = new Audio('sounds/crash.mp3');
+            var crash = new Audio('crash.mp3');
             crash.play();
             break;
 
         case "k":
-            var kick = new Audio('sounds/kick-bass.mp3');
+            var kick = new Audio('kick-bass.mp3');
             kick.play();
             break;
 
         case "l":
-            var snare = new Audio('sounds/snare.mp3');
+            var snare = new Audio('snare.mp3');
             snare.play();
             break;
 
@@ -68,35 +58,35 @@ document.addEventListener("keypress", function (event) {
 function makeSound(key) {
     switch (key) {
         case "w":
-            var tom1 = new Audio('sounds/tom-1.mp3');
+            var tom1 = new Audio('tom-1.mp3');
             tom1.play();
 
         case "a":
-            var tom2 = new Audio('sounds/tom-2.mp3');
+            var tom2 = new Audio('tom-2.mp3');
             tom2.play();
             break;
         case "s":
-            var tom3 = new Audio('sounds/tom-3.mp3');
+            var tom3 = new Audio('tom-3.mp3');
             tom3.play();
             break;
 
         case "d":
-            var tom4 = new Audio('sounds/tom-4.mp3');
+            var tom4 = new Audio('tom-4.mp3');
             tom4.play();
             break;
 
         case "j":
-            var crash = new Audio('sounds/crash.mp3');
+            var crash = new Audio('crash.mp3');
             crash.play();
             break;
 
         case "k":
-            var kick = new Audio('sounds/kick-bass.mp3');
+            var kick = new Audio('kick-bass.mp3');
             kick.play();
             break;
 
         case "l":
-            var snare = new Audio('sounds/snare.mp3');
+            var snare = new Audio('snare.mp3');
             snare.play();
             break;
 
